7f82bf844f0319faec2940690410eb718b6c6587
parunkov <parunkov@gmail.com>
2023-06-30 16:03:48 +0500

    navbar

--- nuxt3-app/app.vue
+++ nuxt3-app/app.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    <NuxtPage />
+    <MainNavbar />
+    <main>
+      <div class="container">
+        <NuxtPage />
+      </div>
+    </main>
   </div>
 </template>
+<script>
+import '@/node_modules/bootstrap/dist/css/bootstrap.min.css';
+import MainNavbar from '@/components/MainNavbar.vue';
+export default {
+  components: {MainNavbar}
+}
+</script>
+<style>
+/* @import url("@/node_modules/bootstrap/dist/css/bootstrap.min.css"); */
+</style>
