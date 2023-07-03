92b15ca16d7132f5afa934207694765d321c3281
parunkov <parunkov@gmail.com>
2023-07-03 12:33:12 +0500

    users on server

--- nuxt3-app/pages/users/index.vue
+++ nuxt3-app/pages/users/index.vue
@@ -1,25 +1,36 @@
 <script setup>
 const openUser = (user) => {
-    const router = useRouter();
-    router.push('/users/' + user);
-}
+  const router = useRouter();
+  router.push("/users/" + user);
+};
 
-const users = ref([]);
+// const users = ref([]);
+const users = await $fetch('https://jsonplaceholder.typicode.com/users').catch((error) => error.data);
 onMounted(async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const result = await response.json();
-    result.forEach((item) => {
-       users.value.push(item) ;
-    });
-})
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  //     const result = await response.json();
+  //     result.forEach((item) => {
+  //        users.value.push(item) ;
+  //     });
+//   const {data: } = await useFetch(
+//     "https://jsonplaceholder.typicode.com/users"
+//   );
+//   res.data.forEach((item) => {
+//     console.log(item);
+//   });
+// const newUsers = await $fetch('https://jsonplaceholder.typicode.com/users').catch((error) => error.data)
+// newUsers.forEach((item) => {
+//     users.value.push(item);
+// });
+});
 </script>
 <template>
-    <section>
-        <h1>User Page</h1>
-        <ul>
-            <li v-for="user of users" :key="user.id">
-                <a href="#" @click.prevent="openUser(user.id)">{{ user.name }}</a>
-            </li>
-        </ul>
-    </section>
+  <section>
+    <h1>User Page</h1>
+    <ul>
+      <li v-for="user of users" :key="user.id">
+        <a href="#" @click.prevent="openUser(user.id)">{{ user.name }}</a>
+      </li>
+    </ul>
+  </section>
 </template>
